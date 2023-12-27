6b4e154aada6b3d8306603367038ea657a989949
eitnanda <152775541+eitnanda@users.noreply.github.com>
2023-12-27 22:44:44 +0900

    Add files via upload

---
+++ Resume.js
@@ -0,0 +1,56 @@
+
+let myResume ={
+    "Personal Information":{
+                    "name": "NANDA KUMAR R",
+                    "email": "eit_nanda@yahoo.co.in",
+                    "phone": 123456789,
+                    "Education": "DME",
+                    "Address" :{
+                    "Door No" : 14/7,
+                    "Steet" : "Main road",
+                    "city": "Erode",
+                    "state": "Tamilnadu",
+                    "country": "India",
+                    "postalCode": 600000,
+        } 
+
+    },
+    "Experience":[
+        {
+     
+                   "company": "Total Machine Solutions pvt Ltd",
+                   "position": "Senior Engineer",
+                   "startDate": "2010-11-04",
+                   "endDate": "Till now",
+                   "summary": "i am Senior manufacturing Engineer for various sector",
+
+        } 
+            ],
+    "Education":
+        {
+                  "institution": "Erode Institute of Technology",
+                  "department": "Mechanical",
+                  "studyType": "fulltime",
+                  "batch start year": 2003,
+                  "batch end year": 2006,
+
+        },
+               
+    
+    "Technical skills":
+                [
+                   "Modelling, Cam Programming"
+                ],
+    "Software skills":
+                [
+                     "Auto Cad,Mastercam, Siemens NX"
+                ],
+    "Languages":
+                [
+                  "Tamil,English",
+                ]
+
+    }
+
+
+console.log (myResume);
